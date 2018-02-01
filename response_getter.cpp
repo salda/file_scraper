a7dce65ae9dadcb7765423cb8816212a590efae4
salda <lazzo@seznam.cz>
2018-02-01 23:11:46 +0100

    fixing indentation broken by some tool

--- response_getter.cpp
+++ response_getter.cpp
@@ -8,7 +8,7 @@ class response_getter { // TODO make singleton?
   char* URL;
 
   static size_t CurlWrite_CallbackFunc_String(void* contents, size_t element_size, size_t element_count, string* output) {
-	  size_t additional_size = element_size * element_count;
+    size_t additional_size = element_size * element_count;
     output->append((char*)contents, additional_size);
     return additional_size;
   }
@@ -22,21 +22,21 @@ public:
     }
     curl_easy_setopt(cURL, CURLOPT_URL, URL);
     curl_easy_setopt(cURL, CURLOPT_FOLLOWLOCATION, 1L);
-	  curl_easy_setopt(cURL, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(cURL, CURLOPT_SSL_VERIFYPEER, 0L);
   }
 
   const string get_response() {
-   	curl_easy_setopt(cURL, CURLOPT_WRITEFUNCTION, CurlWrite_CallbackFunc_String);
+    curl_easy_setopt(cURL, CURLOPT_WRITEFUNCTION, CurlWrite_CallbackFunc_String);
     string response_body;
-	  curl_easy_setopt(cURL, CURLOPT_WRITEDATA, &response_body);
+    curl_easy_setopt(cURL, CURLOPT_WRITEDATA, &response_body);
     CURLcode curl_code = curl_easy_perform(cURL);
     if (curl_code != CURLE_OK) {
       cout << string(curl_easy_strerror(curl_code));
       throw std::runtime_error("failed to perform");
-  	}
-	  long respCode;
-  	curl_easy_getinfo(cURL, CURLINFO_RESPONSE_CODE, &respCode);
-	  if (respCode != 200) {
+    }
+    long respCode;
+    curl_easy_getinfo(cURL, CURLINFO_RESPONSE_CODE, &respCode);
+    if (respCode != 200) {
       cout << "response code from " + string(URL) + ": " + to_string(respCode);
       throw std::runtime_error("response code not 200");
     }
